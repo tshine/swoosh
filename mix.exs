2a01f50efcc4141f8f7f66e82d779b39df03fad2
Steve Domin <steve@stevedomin.com>
2017-08-04 16:29:52 +0300

    Upgrade credo to 0.8

--- mix.exs
+++ mix.exs
@@ -39,7 +39,7 @@ defmodule Swoosh.Mixfile do
      {:gen_smtp, "~> 0.12", optional: true},
      {:cowboy, "~> 1.0", optional: true},
      {:plug, "~> 1.4", optional: true},
-     {:credo, "~> 0.7", only: [:dev, :test]},
+     {:credo, "~> 0.8", only: [:dev, :test]},
      {:bypass, "~> 0.6", only: :test},
      {:ex_doc, "~> 0.15", only: :docs},
      {:inch_ex, ">= 0.0.0", only: :docs}]
