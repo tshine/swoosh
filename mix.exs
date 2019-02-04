c900f4b079114499fad5fa5aed320ba7231d263e
Po Chen <chenpaul914@gmail.com>
2019-02-04 22:17:48 +1100

    remove credo from deps (#338)

--- mix.exs
+++ mix.exs
@@ -51,7 +51,6 @@ defmodule Swoosh.Mixfile do
       {:gen_smtp, "~> 0.12", optional: true},
       {:cowboy, "~> 1.0.1 or ~> 1.1 or ~> 2.4", optional: true},
       {:plug, "~> 1.4", optional: true},
-      {:credo, "~> 1.0", only: [:dev, :test]},
       {:bypass, "~> 1.0", only: :test},
       {:ex_doc, "~> 0.16", only: :docs, runtime: false},
       {:inch_ex, ">= 0.0.0", only: :docs}
