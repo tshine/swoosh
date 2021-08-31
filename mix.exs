5b1f0f3fd7000ac49afd75f02b976b724f7d94be
Po Chen <chenpaul914@gmail.com>
2021-08-31 14:43:55 +1000

    Bump to Elixir 1.10 minimum

--- mix.exs
+++ mix.exs
@@ -8,7 +8,7 @@ defmodule Swoosh.Mixfile do
     [
       app: :swoosh,
       version: @version,
-      elixir: "~> 1.9",
+      elixir: "~> 1.10",
       elixirc_paths: elixirc_paths(Mix.env()),
       consolidate_protocols: Mix.env() != :test,
       build_embedded: Mix.env() == :prod,
