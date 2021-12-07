9752cbf9c1a04b6e6b42ef10dea275600b04312a
Kian Meng Ang <kianmeng@cpan.org>
2021-12-07 09:24:12 +0800

    Update provider options doc for Sendgrid (#643)

--- lib/swoosh/adapters/sendgrid.ex
+++ lib/swoosh/adapters/sendgrid.ex
@@ -30,36 +30,71 @@ defmodule Swoosh.Adapters.Sendgrid do
         use Swoosh.Mailer, otp_app: :sample
       end
 
+  ## Using with provider options
+
+      import Swoosh.Email
+
+      new()
+      |> from({"Xu Shang-Chi", "xu.shangchi@example.com"})
+      |> to({"Katy", "katy@example.com"})
+      |> reply_to("xu.xialing@example.com")
+      |> cc("yingli@example.com")
+      |> cc({"Xu Wenwu", "xu.wenwu@example.com"})
+      |> bcc("yingnan@example.com")
+      |> bcc({"Jon Jon", "jonjon@example.com"})
+      |> subject("Hello, Ten Rings!")
+      |> html_body("<h1>Hello</h1>")
+      |> text_body("Hello")
+      |> put_provider_option(:custom_args, %{
+        my_var: %{my_message_id: 123},
+        my_other_var: %{my_other_id: 1, stuff: 2}
+      })
+      |> put_provider_option(:asm, %{
+        "group_id" => 1,
+        "groups_to_display" => [1, 2, 3]
+      })
+      |> put_provider_option(:categories, ["welcome"])
+      |> put_provider_option(:mail_settings, %{
+        sandbox_mode: %{enable: true}
+      })
+      |> put_provider_option(:tracking_settings, %{
+        subscription_tracking: %{enable: false}
+      })
+
   ## Provider Options
 
   Supported provider options are the following:
 
   #### Inserted into personalization
 
-  #{
-    for(
-      field <- @provider_options_personalization_fields,
-      do: "- " <> inspect(field) <> "\n"
-    )
-    |> Enum.join()
-  }
+    * `:custom_args` (map) - key/value pairs custom arguments that specific to
+      this personalization
+
+    * `:substitutions` (map) - key/value pairs of substitutions string applied
+      to the `:subject` and `:reply-to` parameter
+
+    * `:dynamic_template_data` (map) - key/value pairs of dynamic template data
+      used in Dynamic Transactional Templates, see `:template_id`
 
   #### Inserted into request body
 
-  #{
-    for(
-      field <- @provider_options_body_fields,
-      do: "- " <> inspect(field) <> "\n"
-    )
-    |> Enum.join()
-  }
+    * `:template_id` (string) - an email template ID
+
+    * `:asm` (map) - a map contains fields below on how to handle unsubscribes
+
+    * `:categories` (list[string]) - list of category name for this message
+
+    * `:mail_settings` (map) - collection of mail settings to handle this email
+
+    * `:tracking_settings` (map) - collection of settings to track the metrics
+      of responses of email recipients
 
   ## Sandbox mode
 
-  For [sandbox mode](https://sendgrid.com/docs/for-developers/sending-email/sandbox-mode/),
-  use `put_provider_option/3`:
+  For [sandbox mode](https://sendgrid.com/docs/for-developers/sending-email/sandbox-mode/), use `put_provider_option/3`:
 
       iex> new() |> put_provider_option(:mail_settings, %{sandbox_mode: %{enable: true}})
+
   """
 
   use Swoosh.Adapter, required_config: [:api_key]
