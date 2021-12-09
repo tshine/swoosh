445ceb91989b8505d5fc31d9cf600e6bd28e3f62
Kian Meng Ang <kianmeng@cpan.org>
2021-12-09 09:11:50 +0800

    Add provider options doc for socketlabs (#645)

--- lib/swoosh/adapters/socket_labs.ex
+++ lib/swoosh/adapters/socket_labs.ex
@@ -16,6 +16,44 @@ defmodule Swoosh.Adapters.SocketLabs do
       defmodule Sample.Mailer do
         use Swoosh.Mailer, otp_app: :sample
       end
+
+  ## Using with provider options
+
+      import Swoosh.Email
+
+      new()
+      |> from({"Sisu", "sisu@example.com"})
+      |> to("raya@example.com")
+      |> put_provider_option(:api_template, "12345")
+      |> put_provider_option(:charset, "12345")
+      |> put_provider_option(:mailing_id, "12345")
+      |> put_provider_option(:message_id, "12345")
+      |> put_provider_option(:merge_data, %{
+        "PerMessage" => %{
+          "per_message1" => "value1",
+          "per_message2" => "value2"
+        },
+        "Global" => %{
+          "global1" => "value1",
+          "global2" => "value2"
+        }
+      })
+
+  ## Provider Options
+
+    * `:api_template` (string) - `ApiTemplate`, identifier for a content in the
+      Email Content Manager
+
+    * `:charset` (string) - `Charset`, character set used when creating the
+      email message and default to `UTF8`
+
+    * `:mailing_id` (string) - special header used to track batches of email
+      messages
+
+    * `:message_id` (string) - special header used to track individual message
+
+    * `:merge_data` (map) - data storage for inline Merge feature
+
   """
 
   use Swoosh.Adapter, required_config: [:server_id, :api_key]
