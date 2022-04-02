b0b5ab5f102d06dbb84370930264fbd5f617d88e
tshine <tshine22@gmail.com>
2022-04-02 19:12:35 -0400

    Update mix.exs

--- mix.exs
+++ mix.exs
@@ -64,7 +64,7 @@ defmodule Swoosh.Mixfile do
     [
       {:mime, "~> 1.1 or ~> 2.0"},
       {:jason, "~> 1.0"},
-      {:telemetry, "~> 0.4.2 or ~> 1.0"},
+      {:telemetry, "~> 0.4 or ~> 1.0"},
       {:hackney, "~> 1.9", optional: true},
       {:finch, "~> 0.6", optional: true},
       {:mail, "~> 0.2", optional: true},
